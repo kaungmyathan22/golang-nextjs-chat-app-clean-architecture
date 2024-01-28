d7f7d5b378bf8b5d0a86928ce8d40aa8822ce23e
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-28 22:27:36 +0630

    feat(): db connection setup finished

---
+++ server/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type Database struct {
+	db *sql.DB
+}
+
+func NewDatabase() (*Database, error) {
+	db, err := sql.Open("postgres", "postgresql://root:password@localhost:5433/go-chat?sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("connected to the database....")
+	return &Database{db: db}, nil
+}
+
+func (d *Database) Close() {
+	d.db.Close()
+}
+
+func (d *Database) GetDB() *sql.DB {
+	return d.db
+}
---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module github.com/kaungmyathan22/golang-nextjs-chat-app-clean-architecture
+
+go 1.21.5
+
+require github.com/lib/pq v1.10.9

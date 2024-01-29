b75c9f7c004e802335f5a444817fa9580e3388cc
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-29 13:03:22 +0630

    feat(context): auth provider context adde

---
+++ client/modules/auth_provider.tsx
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/router'
+import { createContext, useEffect, useState } from 'react'
+
+export type UserInfo = {
+  username: string
+  id: string
+}
+
+export const AuthContext = createContext<{
+  authenticated: boolean
+  setAuthenticated: (auth: boolean) => void
+  user: UserInfo
+  setUser: (user: UserInfo) => void
+}>({
+  authenticated: false,
+  setAuthenticated: () => {},
+  user: { username: '', id: '' },
+  setUser: () => {},
+})
+
+const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [authenticated, setAuthenticated] = useState(false)
+  const [user, setUser] = useState<UserInfo>({ username: '', id: '' })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem('user_info')
+
+    if (!userInfo) {
+      if (window.location.pathname != '/signup' && window.location.pathname != '/login') {
+        router.push('/login')
+        return
+      }
+    } else {
+      const user: UserInfo = JSON.parse(userInfo)
+      if (user) {
+        setUser({
+          username: user.username,
+          id: user.id,
+        })
+      }
+      setAuthenticated(true)
+    }
+  }, [authenticated])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        authenticated: authenticated,
+        setAuthenticated: setAuthenticated,
+        user: user,
+        setUser: setUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContextProvider

d166d85ef9e8123c71b6da89c7785f943a2760cb
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-29 12:24:50 +0630

    update(): http router implementation added

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,22 +4,26 @@ import (
 	"log"
 
 	"github.com/kaungmyathan22/golang-nextjs-chat-app-clean-architecture/db"
+	"github.com/kaungmyathan22/golang-nextjs-chat-app-clean-architecture/internal/user"
+	"github.com/kaungmyathan22/golang-nextjs-chat-app-clean-architecture/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("could not initialize database connection: %s", err)
 	}
 
-	// userRep := user.NewRepository(dbConn.GetDB())
-	// userSvc := user.NewService(userRep)
-	// userHandler := user.NewHandler(userSvc)
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
 
 	// hub := ws.NewHub()
 	// wsHandler := ws.NewHandler(hub)
 	// go hub.Run()
 
-	// router.InitRouter(userHandler, wsHandler)
-	// router.Start("0.0.0.0:8080")
+	router.InitRouter(userHandler)
+	// , wsHandler
+	// )
+	router.Start("0.0.0.0:8080")
 }
